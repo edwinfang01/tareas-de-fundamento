c9fead05e3b3b76606fc8d72e12e7f3aadeaa023
edwinfang01 <edwinfang05@gmail.com>
2024-06-07 18:30:28 -0400

    Add files via upload

---
+++ masa coporal.py
@@ -0,0 +1,7 @@
+peso = float( input('ingrese su peso') )
+unidad = float( input('diga en que unidad ingreso su peso (1/kg) (2/lb)').strip() )
+altura = float( input('ingrese su altura en metros') )
+if unidad == 2:
+    peso = peso * (1/2.20462262) ## lb a kg
+masa = peso / (altura**2)
+print(masa)
